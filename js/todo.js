4494f5ba38fd802b3ce7d693069bcd2197967134
DaraKapl <darakapl420@gmail.com>
2023-11-19 23:24:19 +0500

    hyi

--- js/todo.js
+++ js/todo.js
@@ -1,28 +1,65 @@
-window.onload = function () {
-  var todoList = [];
-
-  document.getElementById("add").onclick = function () {
-    var d = document.getElementById("in").value;
-
-    var temp = {};
-    temp.todo = d;
-    temp.check = false;
-    var i = todoList.length;
-    todoList[i] = temp;
-    console.log(todoList);
-    out();
-  };
-  function out() {
-    var out = "";
-    for (var key in todoList) {
-      // if (todoList[key].check == true) {
-      //   out += "<input type='checkbox'checked>";
-      // } else {
-      //    out += "<input type='checkbox'>";
-      // }
-      out += todoList[key].todo + "<br>";
-    }
-    document.getElementById("out").innerHTML = out;
-  }
-  total.textContent = i;
+const id = (x) => {
+  return document.getElementById(x);
 };
+let input = id("input");
+let addBtn = id("btn");
+let listContainer = id("list");
+let itemList = localStorage.itemList ? JSON.parse(localStorage.itemList) : [];
+
+const renderList = () => {
+  listContainer.innerHTML = ``;
+  for (let i = itemList.length - 1; i > 0; i--) {
+    listContainer.innerHTML += `
+            <div class="list-item">
+                <div class="item-name" >
+                    ${itemList[i]}
+                </div>
+                <div class="nameList">
+                    <button onclick="deathItem(${i})">Убит</button>
+                    <button onclick="deleteItem(${i})">Прощен</button>
+                </div>
+            </div>
+        `;
+  }
+};
+
+const addEvent = () => {
+  let value = input.value;
+  if (value.length > 0) {
+    itemList.push(value);
+    input.value = "";
+  } else {
+    alert("Please specify a name for your task");
+  }
+  localStorage.itemList = JSON.stringify(itemList);
+  renderList();
+};
+
+const deleteItem = (index) => {
+  let item = itemList[index];
+  if (item != undefined) {
+    itemList.splice(index, 1);
+    localStorage.itemList = JSON.stringify(itemList);
+    renderList();
+  } else {
+    alert("Item has already been deleted.");
+  }
+};
+const deathItem = (index) => {
+  let item = itemList[index];
+  if (item != undefined) {
+    //zdv
+
+    //cscz
+    localStorage.itemList = JSON.stringify(itemList);
+    renderList();
+  } else {
+    alert("Item has already been deleted.");
+  }
+};
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addEvent();
+});
+
+renderList();
